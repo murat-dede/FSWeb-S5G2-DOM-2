8b7164abe5ca0be55cdd832bb00c1742b34ec4f2
Emre Sahiner <emre@journeyapp.com>
2022-11-22 23:58:05 +0300

    etüdde yaptıklarımız

--- README.md
+++ README.md
@@ -27,15 +27,15 @@ Bu projede sakın **hiçbir dosyayı taşımayın veya adını değiştirmeyin**
 * [ ] [index.js dosyası](src/index.js)'nı kullanarak, en az 10 _farklı_ tipte [event listeners](https://developer.mozilla.org/en-US/docs/Web/Events) oluşturun. Bu siteyi interaktif hale getirmek için kendi yaratıcılığınızı kullanın. Örnek, eventlerle renkleri değiştirebilir, nesnelere animasyonlar ekleyebilir, DOM elementleri ekleyebilir veya silebilirsiniz.
 
 * [ ] Kullanabileceğiniz bazı event çeşitleri şunlardır:
-  * [ ] `mouseover`
-  * [ ] `keydown`
-  * [ ] `wheel`
-  * [ ] `load`
-  * [ ] `focus`
+  * [X] `mouseover`
+  * [X] `keydown`
+  * [X] `wheel`
+  * [x] `load`
+  * [x] `focus`
   * [ ] `resize`
   * [ ] `scroll`
   * [ ] `select`
-  * [ ] `dblclick`
+  * [X] `dblclick`
   * [ ] `drag / drop`
 
 Not: Drag/drop diğerlerine göre biraz daha alengirli. Bu aslında tek bir tür event türü değil, birkaç farklı tür eventin bir arada çalışması gerekir.
--- src/index.js
+++ src/index.js
@@ -7,3 +7,83 @@ import './less/index.less'
 
 
 // Kodlar buraya gelecek!
+
+const ilkNavItem = document.querySelector('#redAlan');
+
+window.addEventListener("load", (event) => {
+    ilkNavItem.style.display = "none";
+  });
+
+const tumResimler = document.querySelectorAll('img');
+tumResimler.forEach((resim) => {
+        resim.addEventListener("dblclick", (event) => {
+            resim.style.filter = "grayscale(100%)";
+        })
+    }   
+)
+
+tumResimler.forEach((resim) => {
+    resim.addEventListener("mouseover", (event) => {
+        resim.style.filter = "unset";
+    })
+}   
+)
+
+tumResimler.forEach((resim) => {
+    resim.addEventListener("wheel", (event) => {
+
+        event.preventDefault();
+
+        scale += event.deltaX * -0.01;
+    
+        // Restrict scale
+        scale = Math.min(Math.max(.125, scale), 4);
+    
+        // Apply scale transform
+        resim.style.transform = `scale(${scale})`;
+
+    })
+})
+    
+  let scale = 1;
+
+const input = document.querySelector('input');
+const log = document.getElementById('log');
+const tumButonlar = document.querySelectorAll('.btn');
+
+input.addEventListener('keydown', logKey);
+
+function logKey(e) {
+    if (e.key == 1) {
+        tumButonlar[0].style.background = "red";
+    } else if (e.key == 2) {
+        tumButonlar[1].style.background = "red";
+    } else if (e.key == 3) {
+        tumButonlar[2].style.background = "red";
+
+    }
+  //log.textContent += ` ${e.key}`;
+
+}
+
+input.addEventListener('keyup', (e) => {
+    if (e.key == 3) {
+        input.setAttribute("disabled", "disabled");
+    }
+});
+
+input.addEventListener('focus', (e) => {
+
+        input.style.background = "red";
+
+});
+
+window.addEventListener('resize', (e)=>{
+    log.textContent = window.innerWidth;
+
+    if ( window.innerWidth <1000) {
+        ilkNavItem.style.display = "block";
+    } else {
+        ilkNavItem.style.display = "none";
+    }
+})

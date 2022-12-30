3006a0f82f4d69845b948e539c6cb20516263c00
murat-dede <murat1_dede@hotmail.com>
2022-12-30 15:45:02 +0300

    dom 2

--- src/index.js
+++ src/index.js
@@ -7,3 +7,35 @@ import './less/index.less'
 
 
 // Kodlar buraya gelecek!
+
+const images = document.querySelectorAll('.drags img')
+
+images.forEach((img) => {
+img.addEventListener('dragstart'= (ev)=> {
+    ev.dataTransfer.setData('text',ev.target.src); 
+})
+    
+})
+
+const mainImg = document.querySelectorAll('header img');
+
+mainImg.addEventListener('drop',(ev)=>{
+    ev.preventDefault();
+    const targetImg = document.getElementById(ev.target.id);
+    const targetImgSrc=targetImg.src ;
+    
+    const sourceImg = document.getElementById(ev.dataTransfer.getData('text'));
+    const sourceImgSrc=targetImg.src ;
+
+    targetImg.src=sourceImgSrc;
+    sourceImg.src=targetImgSrc;
+   
+    
+
+
+});
+mainImg.addEventListener('dragover',(ev)=>{
+    ev.preventDefault();
+
+})
+
